a44b22f3b38444fa8b3507a026d543086bc1dd74
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-02-21 13:49:01 -0300

    Render JSON with associations

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -5,8 +5,16 @@ class Contact < ApplicationRecord
 		"Pedro"
 	end
 
+	def kind_description
+		self.kind.description
+	end
+
 	def as_json(options={})
-		super(methods: :author, root: true)
+		super(
+			root: true, 
+			methods: [:author, :kind_description],
+			include: { kind: { only: [:description] }}
+			)
 	end
 
 end
