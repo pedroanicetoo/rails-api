0990ba8704588cc94a3c353ba6556cfada2d9a1c
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-02-28 14:22:59 -0300

    Address CRUD has_one association

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
+
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
