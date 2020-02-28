8fb54d64dcffb594a7058258801eb46be62dba92
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-02-28 15:59:55 -0300

    has_many CRUS for phones

--- notebook-api/app/serializers/address_serializer.rb
+++ notebook-api/app/serializers/address_serializer.rb
@@ -1,3 +1,8 @@
 class AddressSerializer < ActiveModel::Serializer
   attributes :id, :city, :street
+
+  belongs_to :contact do
+  	link(:related) { contact_address_url(object.contact.id) }
+  end
+
 end
