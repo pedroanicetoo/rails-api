e5f9bb0175282a180072d0507808b4d7c3c783e4
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-10 14:15:43 -0300

    devise_token_auth example (with kinds model) on devise token

--- notebook-api/app/controllers/kinds_controller.rb
+++ notebook-api/app/controllers/kinds_controller.rb
@@ -1,5 +1,5 @@
 class KindsController < ApplicationController
-
+  before_action :authenticate_user!
   # TOKEN = "secret123"
 
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
@@ -8,8 +8,8 @@ class KindsController < ApplicationController
   # include ActionController::HttpAuthentication::Digest::ControllerMethods
   # USERS = { "pedro" => Digest::MD5.hexdigest(["pedro","Application","secret"].join(":"))}
 
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-  before_action :authenticate
+  # include ActionController::HttpAuthentication::Token::ControllerMethods
+  # before_action :authenticate
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -65,19 +65,19 @@ class KindsController < ApplicationController
       params.require(:kind).permit(:description)
     end
 
-    def authenticate
-      # authenticate_or_request_with_http_digest("Application") do |username|
-      #   USERS[username]
-      # end
-      authenticate_or_request_with_http_token do |token, options|
-        hmac_secret = 'my$ecretK3y'
-        JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
-        # Compare the tokens in a time-constant manner, to mitigate
-        # timing attacks.
-        # ActiveSupport::SecurityUtils.secure_compare(
-        #   ::Digest::SHA256.hexdigest(token),
-        #   ::Digest::SHA256.hexdigest(TOKEN)
-        #   )
-      end
-    end
+    # def authenticate
+    #   # authenticate_or_request_with_http_digest("Application") do |username|
+    #   #   USERS[username]
+    #   # end
+    #   authenticate_or_request_with_http_token do |token, options|
+    #     hmac_secret = 'my$ecretK3y'
+    #     JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+    #     # Compare the tokens in a time-constant manner, to mitigate
+    #     # timing attacks.
+    #     # ActiveSupport::SecurityUtils.secure_compare(
+    #     #   ::Digest::SHA256.hexdigest(token),
+    #     #   ::Digest::SHA256.hexdigest(TOKEN)
+    #     #   )
+    #   end
+    # end
 end
--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resource :auths, only: [:create]
   resources :kinds
   resources :contacts do
