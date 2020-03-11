c7e6ec645f5b64fd4c8f99a7c0e6dcf8ad1894b2
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-11 14:17:30 -0300

    versionamento com hostname ou subdomínio

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,40 +1,43 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  # resource :auths, only: [:create]
+  # resources :auths, only: [:create]
   resources :kinds
 
-  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1.0"}) do
-    resources :contacts do
-      resource :kind, only: [:show]
-      resource :kind, only: [:show], path: 'relationships/kind'
+  constraints subdomain: 'v1' do
+    scope module: 'v1' do
+      resources :contacts do
+        resource :kind, only: [:show]
+        resource :kind, only: [:show], path: 'relationships/kind'
 
-      resource :phones, only: [:show]
-      resource :phones, only: [:show], path: 'relationships/phones'
+        resource :phones, only: [:show]
+        resource :phones, only: [:show], path: 'relationships/phones'
 
-      resource :phone, only: [:update, :create, :destroy]
-      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+        resource :phone, only: [:update, :create, :destroy]
+        resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+        resource :address, only: [:show, :update, :create, :destroy]
+        resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      end
     end
   end
 
-    api_version(:module => "v2", :header => {:name => "X-Version", :value => "2.0"}) do
-    resources :contacts do
-      resource :kind, only: [:show]
-      resource :kind, only: [:show], path: 'relationships/kind'
+  constraints subdomain: 'v2' do
+    scope module: 'v2' do
+      resources :contacts do
+        resource :kind, only: [:show]
+        resource :kind, only: [:show], path: 'relationships/kind'
 
-      resource :phones, only: [:show]
-      resource :phones, only: [:show], path: 'relationships/phones'
+        resource :phones, only: [:show]
+        resource :phones, only: [:show], path: 'relationships/phones'
 
-      resource :phone, only: [:update, :create, :destroy]
-      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+        resource :phone, only: [:update, :create, :destroy]
+        resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+        resource :address, only: [:show, :update, :create, :destroy]
+        resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      end
     end
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
