bcee30205372aabbe55d6c510bb6f5d5214cbdfd
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-11 14:26:44 -0300

    Versionamento utilizando segmento de URL

---
+++ notebook-api/app/serializers/v1/contact_serializer.rb
@@ -0,0 +1,36 @@
+module V1
+  class ContactSerializer < ActiveModel::Serializer
+    attributes :id, :name, :birthdate
+
+    # associations
+    belongs_to :kind do
+      link(:related) { v1_contact_kind_url(object.id) }
+    end
+
+    has_many :phones do
+      link(:related) { v1_contact_phones_url(object.id) }
+    end 
+    
+    has_one :address do 
+      link(:related) { v1_contact_address_url(object.id) }
+    end
+
+    # meta do
+    #   { author: "Pedro Pereira"}
+    # end
+
+    def attributes(*args)
+      h = super(*args)
+      # pt-BR
+      # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+      # h[:created_at] = (I18n.l(object.created_at) unless object.created_at.blank?)
+      # h[:updated_at] = (I18n.l(object.created_at) unless object.updated_at.blank?)
+      h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+      h[:created_at] = object.created_at.to_time.iso8601 unless object.created_at.blank?
+      h[:updated_at] = object.updated_at.to_time.iso8601 unless object.updated_at.blank?
+      h
+    end
+
+  end
+
+end
---
+++ notebook-api/app/serializers/v2/contact_serializer.rb
@@ -0,0 +1,19 @@
+module V2
+	class ContactSerializer < ActiveModel::Serializer
+	  attributes :id, :name
+
+	  belongs_to :kind do
+	    link(:related) { v2_contact_kind_url(object.id) }
+	  end
+
+	  has_many :phones do
+	    link(:related) { v2_contact_phones_url(object.id) }
+	  end 
+	  
+	  has_one :address do 
+	    link(:related) { v2_contact_address_url(object.id) }
+	  end
+
+	end
+
+end
